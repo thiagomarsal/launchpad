c865b0bd4a237215bd64ecdbcb017b89c161bbf1
Farias, Thiago <thiagomarsal.farias@gmail.com>
2025-01-18 08:21:15 -0700

    adding liferay portal deployment

---
+++ kubernetes/liferay-k3s/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# deploy
+kubectl create -f 00-liferay-configs.yaml
+
+./01-create-configmaps.sh
+
+kubectl create -f 02-liferay-mysql.yaml
+kubectl create -f 03-liferay-search.yaml
+kubectl create -f 04-liferay-portal.yaml
+# kubectl create -f 99-liferay-nginx.yaml
+kubectl create -f 05-liferay-ingress.yaml
+#kubectl create -f 06-nginx-ingress.yaml
+
